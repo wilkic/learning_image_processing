8fcff1f1baae0491d4b693c330aadfc9768beaf2
wilkic <wilkic@gmail.com>
2016-08-11 01:49:32 -0400

    Improving frame grab script... adding cam that seems to be working

--- tutorials/stream/grab_stream.py
+++ tutorials/stream/grab_stream.py
@@ -1,20 +1,24 @@
 
 import cv2
 import time
+import math
 
-vcap = cv2.VideoCapture('rtsp://108.45.109.111:561/live0.264')
+#vcap = cv2.VideoCapture('rtsp://108.45.109.111:561/live0.264')
+vcap = cv2.VideoCapture('rtsp://108.45.109.111:557/live0.264')
 
 i = 0
 while i < 10:
     ret, frame = vcap.read()
     
-    t = time.localtime()
-    ts = time.strftime( '%Y%m%dT%H%M%S.%f', t )
-    tss = time.strftime( '%Y%m%dT%H%M%S', t )
+    t = time.time()
+    tl = time.localtime(t)
+    ms = (t - math.floor(t))*1000
+    ts = time.strftime( '%Y%m%dT%H%M%S', tl )
+    tss = ts + ('.%d' % ms)
     
-    print 'Writing frame %d at %s' % (i,ts)
+    print 'Writing frame %d at %s' % (i,tss)
 
-    cv2.imwrite("frame_%s.jpg" % tss, frame)
+    cv2.imwrite("frame_%s.jpg" % ts, frame)
     
     i += 1   
 
