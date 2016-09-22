170710d80583491ab5a345eb61dbba39d3369b67
wilkic <wilkic@gmail.com>
2016-09-22 02:20:03 -0400

    Messing around with thresholds and added a bounding box

--- tutorials/opencv/contour.py
+++ tutorials/opencv/contour.py
@@ -3,7 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 #filename = 'van_spot17.jpg'
-filename = '/home/acp/Downloads/spot37_occupied.jpg'
+#filename = 'home/acp/Downloads/spot37_occupied.jpg'
+filename = '/home/acp/work/camera_testing/hosafe/cam9/tess.bmp'
 
 im = cv2.imread(filename)
 
@@ -18,8 +19,11 @@ thc = th1.copy()
 thc, contours, hierarchy = cv2.findContours(thc,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 
-blur = cv2.GaussianBlur(imgray,(5,5),0)
+#blur = cv2.GaussianBlur(imgray,(5,5),0)
+blur = cv2.GaussianBlur(imgray,(3,3),0)
+#blur = imgray.copy()
 ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+#th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
 th3c = th3.copy()
 th3c, contours3, hierarchy = cv2.findContours(th3c,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -50,6 +54,7 @@ lmax = 0
 Ls = np.zeros(len(contours3))
 Ss = np.zeros(len(contours3))
 Ps = np.zeros(len(contours3))
+CMs = np.zeros((len(contours3),2))
 
 for i,c in enumerate(contours3):
     l = len(c)
@@ -60,16 +65,26 @@ for i,c in enumerate(contours3):
     Ls[i] = l
     Ss[i] = cv2.contourArea(c)
     Ps[i] = cv2.arcLength(c,True)
+    M = cv2.moments(c)
+    if np.abs(M['m00']) > 5e-14:
+        CMs[i,0] = int(M['m10']/M['m00'])
+        CMs[i,1] = int(M['m01']/M['m00'])
+
 
 imC = im.copy()
 cv2.drawContours(imC, [bigc], -1, (200,0,50), 3)
 
+imR = im.copy()
+x,y,w,h = cv2.boundingRect(bigc)
+cv2.rectangle(imR,(x,y),(x+w,y+h),(0,255,0),2)
 
-images = [im, th1, thc, imc, th3, im3c, imC]
+
+images = [im, th1, thc, imc, th3, im3c, imC, imR]
 nr = 2
 nc = np.ceil(float(len(images))/nr)
 for i,im in enumerate(images):
     plt.subplot(nr,nc,i+1)
     plt.imshow(im,'gray')
 plt.show()
+
 
