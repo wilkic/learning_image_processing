50d5f1cee3061fbbb7aa7af7e7d48acbfc8241d5
wilkic <wilkic@gmail.com>
2019-01-18 23:17:08 -0500

    Updating table maker now that we have no space numbers

--- tutorials/get_bpark_payers.py
+++ tutorials/get_bpark_payers.py
@@ -14,29 +14,9 @@ import requests
 import html_ops as ho
 
 
-# Set up the spots dictionary
-nSpots = 22
-
-# These spots are monthly
-monthlies = []
-
-defaultProperties = {
-    'paid': 0,
-    'occupied': 0,
-    'startTime': '',
-    'endTime': '',
-    'lps': '',
-    'lpn': '',
-    'monthly': 0
-}
-
-spots = {prop:defaultProperties.copy() for prop in range(1,nSpots+1)}
-
-# Assign the monthlies
-for i in monthlies:
-    spots[i]['monthly'] = 1
-    spots[i]['paid'] = 1
-
+# Number of available spots
+nSpots = 16
+nPaidSpots = 0
 
 # Get the PM API response
 zone = 3127
@@ -45,11 +25,42 @@ usr = 'ws_goodspeedcapi'
 pwd = 'x2warEya'
 resp = requests.get(url, auth=(usr,pwd), verify=True)
 
-# Populate spots based on response
+
+# Make a header row for table of data from API call
+tabHtml = '<table border="1">'
+tabHtml += ("<tr><td>Paid Start Time</td>"
+                "<td>Paid End Time</td>"
+                "<td>License Number</td>"
+                "<td>License State</td>"
+            "</tr>")
+
+# Parse API response JSON
 if resp.status_code != 404:
     data = resp.json()
     
-    outdir = '/mnt/data/catch/bpark/get_payer_output'
+    # Default row to white
+    rcolor = '#FFFFFF'
+    rowsty = 'style="background-color:%s"' % rcolor
+    
+    # Populate row of table from API response
+    for i in data['parkingRights']:
+    
+        row = '<tr %s>' % rowsty
+        pstCell = '<td> ' + str(i['startDateLocal']) + '</td>'
+        petCell = '<td> ' + str(i['endDateLocal']) + '</td>'
+        lpnCell = '<td> ' + str(i['lpn']) + '</td>'
+        lpsCell = '<td> ' + str(i['lpnState']) + '</td>'
+        row = row + pstCell + petCell
+        row = row + lpnCell + lpsCell
+        row += '</tr>'
+        tabHtml += row
+
+        nPaidSpots += 1
+
+
+    # Log raw API results 
+    #outdir = '/mnt/data/catch/bpark/get_payer_output'
+    outdir = '/tmp/get_payer_output'
 
     if not os.path.exists(outdir):
         os.makedirs(outdir)
@@ -60,67 +71,21 @@ if resp.status_code != 404:
         print >> out, dt.datetime.now()
         pp.pprint( data, stream=out )
 
-    for i in data['parkingRights']:
-        sn = int( i['spaceNumber'] )
-        if sn <= len(spots):
-            spots[ sn ]['paid'] = 1
-            spots[ sn ]['startTime'] = str(i['startDateLocal'])
-            spots[ sn ]['endTime'] = str(i['endDateLocal'])
-            spots[ sn ]['lpn'] = str(i['lpn'])
-            spots[ sn ]['lps'] = str(i['lpnState'])
 
-
-# Put the data in a table
-tabHtml = '<table border="1">'
-tabHtml += ("<tr><td>Space Number</td>"
-                "<td>Occupied</td>"
-                "<td>Paid</td>"
-                "<td>Paid Start Time</td>"
-                "<td>Paid End Time</td>"
-                "<td>License Number</td>"
-                "<td>License State</td>"
-                "<td>Monthly</td>"
-            "</tr>")
-
-
-n_remaining = nSpots
-
-for spot in spots:
-    
-    # For now, update remaining number of
-    # spots based on the number paid
-    pd = spots[spot]['paid']
-    n_remaining -= pd
-
-    # Default row to white
-    rcolor = '#FFFFFF'
-
-    # Mark paid spots as green
-    if pd:
-	rcolor = '#00FF00"'
-    
-    rowsty = 'style="background-color:%s"' % rcolor
-    row = '<tr %s>' % rowsty
-    spaceCell = '<td>Space ' + str(spot) + '</td>'
-    occCell = '<td> ' + str(spots[spot]['occupied']) + '</td>'
-    paidCell = '<td> ' + str(spots[spot]['paid']) + '</td>'
-    pstCell = '<td> ' + str(spots[spot]['startTime']) + '</td>'
-    petCell = '<td> ' + str(spots[spot]['endTime']) + '</td>'
-    lpnCell = '<td> ' + str(spots[spot]['lpn']) + '</td>'
-    lpsCell = '<td> ' + str(spots[spot]['lps']) + '</td>'
-    mnthCell = '<td> ' + str(spots[spot]['monthly']) + '</td>'
-    row += spaceCell 
-    row = row + occCell + paidCell
-    row = row + pstCell + petCell
-    row = row + lpnCell + lpsCell
-    row += mnthCell
-    row += '</tr>'
-    tabHtml += row
-
+# Finish the table
 tabHtml += '</table>'
 
-ho.write_page( 'bpark_table.html', 'Available Spots', 60, tabHtml )
-os.rename("bpark_table.html","/var/www/html/bpark/table/index.html")
+# Write the full page html
+ho.write_page( 'bpark_table.html', 'Paid Spots', 60, tabHtml )
+
+# Move it to the right spot
+#os.rename("bpark_table.html","/var/www/html/bpark/table/index.html")
+os.rename("bpark_table.html","/tmp/bptab.html")
+
+######################
+
+# How many unpaid spots remain
+n_remaining = nSpots - nPaidSpots
 
 nHtml = """\
         <div>
@@ -131,6 +96,7 @@ nHtml = """\
         """ % n_remaining 
 
 ho.write_page( 'n_bpark_avail.html', 'Available Spots', 300, nHtml )
-os.rename("n_bpark_avail.html","/var/www/html/bpark/n_spots_not_paid/index.html")
+#os.rename("n_bpark_avail.html","/var/www/html/bpark/n_spots_not_paid/index.html")
+os.rename("n_bpark_avail.html","/tmp/nsnp.html")
 
 
