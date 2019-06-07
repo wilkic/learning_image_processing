521b39d88c60483f4ebb521299782d5e4b7c9e43
wilkic <wilkic@gmail.com>
2019-06-06 23:28:22 -0400

    Updating all the latest notes
    adding an uploader for ap

---
+++ tutorials/remote_cam_upload/cam_passes.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+DATETIME=`date +%Y%m%d%H%M%S`
+
+dest='ubuntu@ec2-54-172-171-223.compute-1.amazonaws.com:/mnt/data/catch/images_and_timestamps/.'
+
+# All cam ips start at 101 and go up from there
+ip_base='10.10.110.1'
+n_cams=15
+
+# New cams have different url for accessing image
+new=(1 10 12 14 15)
+
+# 3,4,5,6,7,11 don't work
+# 2, 8, 9, 13 are old
+
+
+##############
+
+sp="  "
+es="';"
+
+ips=()
+for (( i=1; i<$n_cams+1; i++ )); do
+    ip=$(printf "%s%02d\n" $ip_base $i)
+    echo "$ip"
+    ips+=($ip)
+    
+    # Assume it's an old url
+    url='http://'$ip'/cgi-bin/getsnapshot.cgi'
+
+    # Check if new, change url accordingly
+    for j in ${new[@]}; do
+        if [ $i == $j ]; then
+            url='http://admin:admin@'$ip'/tmpfs/auto.jpg'
+        fi
+    done
+
+    echo "Fetching snap from $ip"
+    echo "... $url"
+    wget $url -O cam$i.jpg
+    
+    DATE=`date +%F`
+    TIME=`date +%T`
+    ss="let text$i = '"
+    echo $ss$DATE$sp$TIME$es > ts$i.js
+    
+    scp -i "ggp1.pem" cam$i.jpg ts$i.js $dest
+
+
+done
+
+
+##rtsp_port='554'
+##rurl='rtsp://'$ip':'$rtsp_port'/live0.264'
+##ffmpeg -r 25 -y -i $rurl -updatefirst 1 -r 2 cam.bmp
+
