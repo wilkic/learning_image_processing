ef26463d2c6f706d8b1443595dfe135db4f87396
wilkic <wilkic@gmail.com>
2020-04-05 21:44:24 -0400

    Adding script to make static address assignments for cams

---
+++ tutorials/make_cam_assigns.py
@@ -0,0 +1,17 @@
+
+fname = 'camera_assignments.txt'
+fdir = '/home/c/gsp/learning_image_processing/tutorials/notes/copi_setup/'
+
+ffname = fdir + fname
+
+with open( ffname, 'a+' ) as out:
+    for i in range(26,55):
+        mout  = "host cam_%d {\n" % i
+        mout += '    option hostname "Camera%d";\n' %i
+        mout += '    hardware ethernet ;\n'
+        mout += '    fixed-address 10.10.110.1%d";\n' %i
+        mout += '}\n'
+
+        print >> out, mout
+
+
