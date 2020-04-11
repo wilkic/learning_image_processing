0945793b5db4a0ebf1f8f1db2b15edd6d58728e2
wilkic <wilkic@gmail.com>
2020-04-11 01:30:34 -0400

    fixing ANOTHER typo

--- tutorials/make_cam_assigns.py
+++ tutorials/make_cam_assigns.py
@@ -7,7 +7,7 @@ ffname = fdir + fname
 with open( ffname, 'a+' ) as out:
     for i in range(26,55):
         mout  = "host cam_%d {\n" % i
-        mout += '    option hostname "Camera%d";\n' %i
+        mout += '    option host-name "Camera%d";\n' %i
         mout += '    hardware ethernet ;\n'
         mout += '    fixed-address 10.10.110.1%d;\n' %i
         mout += '}\n'
